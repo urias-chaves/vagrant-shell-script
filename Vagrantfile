14be5a6570ba92e7d5785081964281c5fbd46d8d
urias-chaves <uriaschavesmg@hotmail.com>
2023-08-01 18:49:13 -0300

    Upload finalizado

--- Vagrantfile
+++ Vagrantfile
@@ -1,8 +1,8 @@
 Vagrant.configure("2") do |config|
    config.vm.box = "ubuntu/focal64"
-   config.vm.network "public_network"
-  config.vm.provider "virtualbox" do |vb|
-    vb.nome = "vagrant-shell-script"
+   config.vm.provider "virtualbox" do |vb|
+    vb.name = "vagrant-shell-script"
   end
+   config.vm.network "public_network"
   config.vm.provision "shell", path:"install.sh"
 end
--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-
+echo "Atualizando e instalando os pacotes no sistema."
 apt update
-
+apt upgrade
 apt-get install -y vim curl telnet unzip wget net-tools htop nmap
 
-apt upgrade
+#####################################################FIM##########################################
+
+echo "Criando o usuário e listando o arquivo passwd."
+useradd -m urias
+cat /etc/passwd
+####################################################FIM##########################################
